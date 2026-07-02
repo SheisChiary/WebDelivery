26e16147914f22923aeacd6abceece0120765fdd
Bassma4 <boubassma9@gmail.com>
2026-07-02 17:21:09 +0200

    css

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -24,7 +24,7 @@ public class Utente {
     @Column(nullable = false)
     private String ruolo;
 
-    @Column(name = "nome_completo",nullable = false)
+    @Column(nullable = false)
     private String nome;
 
     @Column(nullable = false)
@@ -41,30 +41,57 @@ public class Utente {
     }
 
    
-    public Long getId() { return id;}
-    public void setId(Long id) { this.id = id;}
+    public Long getId() {
+        return id;
+    }
 
-    public String getEmail() { return email;}
-    public void setEmail(String email) { this.email = email;}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getPassword() { return password;}
-    public void setPassword(String password) { this.password = password;}
+    public String getEmail() {
+        return email;
+    }
 
-    public String getRuolo() { return ruolo;}
-    public void setRuolo(String ruolo) { this.ruolo = ruolo;}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getNome() { return nome;}
-    public void setNome(String nome) { this.nome = nome;}
+    public String getPassword() {
+        return password;
+    }
 
-    public String getTelefono() { return telefono;}
-    public void setTelefono(String telefono) { this.telefono = telefono;}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
     
-    public String getIndirizzo() { return indirizzo;}
-    public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo;}
-    
-    public String getCognome() { return cognome;}
-    public void setCognome(String indirizzo) { this.cognome = cognome;}
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
 
-
-
+    public String getIndirizzo() { return indirizzo; }
+    public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
 }
